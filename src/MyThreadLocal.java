00fde62c564f51464844f37d1a27f4381fab7737
KosmA <kkolodich@yandex.ru>
2021-09-22 20:43:40 +0400

    1

--- src/MyThreadLocal.java
+++ src/MyThreadLocal.java
@@ -1,9 +1,19 @@
+import java.lang.ref.WeakReference;
 import java.util.Map;
 import java.util.WeakHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 class MyThreadLocal<T> {
+
+    private final int threadLocalHashCode = nextHashCode();
+    private static final AtomicInteger nextHashCode = new AtomicInteger();
+    private static final int HASH_INCREMENT = 0x61c88647;
+
+    private static int nextHashCode() {
+        return nextHashCode.getAndAdd(HASH_INCREMENT);
+    }
 
     private MyThreadLocalSingleton<Object> myThreadLocalSingleton;
 
@@ -12,17 +22,22 @@ class MyThreadLocal<T> {
     }
 
     public T get() {
-        return (T) myThreadLocalSingleton.get();
+        return (T) myThreadLocalSingleton.get(this);
     }
 
     public void set(T value) {
-        myThreadLocalSingleton.set(value);
+        myThreadLocalSingleton.set(value, this);
+    }
+
+    public void remove() {
+        myThreadLocalSingleton.remove(this);
     }
 
     public static class MyThreadLocalSingleton<T> {
 
+        private static final int INITIAL_CAPACITY = 16;
         private static volatile MyThreadLocalSingleton<Object> instance;
-        private static final Map<Thread, Object> map = new WeakHashMap<>();;
+        private static final Map<Thread, Entry[]> map = new WeakHashMap<>();
         private static final ReentrantReadWriteLock rwl = new ReentrantReadWriteLock();
         private static final Lock r = rwl.readLock();
         private static final Lock w = rwl.writeLock();
@@ -43,15 +58,19 @@ class MyThreadLocal<T> {
             }
         }
 
-        public T get() {
+        public T get(MyThreadLocal<?> myThreadLocal) {
             r.lock();
             try {
                 T value = null;
                 Thread t = Thread.currentThread();
-                //int i = t.getName().hashCode();
-                Object result = map.get(t);
+                Entry[] result = map.get(t);
                 if (result != null) {
-                    value = (T) result;
+                    int i = myThreadLocal.threadLocalHashCode & (result.length - 1);
+                    Entry e = result[i];
+                    if (e != null && e.get() == myThreadLocal) {
+                        value = (T) result[i].value;
+                    } else
+                        return getEntryAfterMiss(myThreadLocal, i, e, result);
                 }
                 return value;
             } finally {
@@ -59,25 +78,108 @@ class MyThreadLocal<T> {
             }
         }
 
-        public void set(T value) {
+        private T getEntryAfterMiss(MyThreadLocal<?> key, int i, Entry e, Entry[] table) {
+            int len = table.length;
+            while (e != null) {
+                MyThreadLocal<?> k = e.get();
+                if (k == key)
+                    return (T) e.value;
+                else
+                    i = nextIndex(i, len);
+                e = table[i];
+            }
+            return null;
+        }
+
+        public void set(T value, MyThreadLocal<?> myThreadLocal) {
             w.lock();
             try {
                 Thread t = Thread.currentThread();
-                //int i = t.getName().hashCode();
-                map.put(t, value);
+                int len;
+                int i;
+                Entry[] tab = map.get(t);
+                if (tab != null) {
+                    len = tab.length;
+                    i = myThreadLocal.threadLocalHashCode & (len - 1);
+                    for (Entry e = tab[i]; e != null; e = tab[i = nextIndex(i, len)]) {
+                        MyThreadLocal<?> k = e.get();
+
+                        if (k == myThreadLocal) {
+                            e.value = value;
+                            return;
+                        }
+                    }
+                    int count = 0;
+                    for (int j = 0; j < len; j++) {
+                        if (tab[j] != null) {
+                            count++;
+                        }
+                    }
+                    if (count >= len * 2 / 3) {
+                        tab = resize(tab);
+                        i = myThreadLocal.threadLocalHashCode & (tab.length - 1);
+                    }
+                    tab[i] = new Entry(myThreadLocal, value);
+                    map.put(t, tab);
+                } else {
+                    tab = new Entry[INITIAL_CAPACITY];
+                    i = myThreadLocal.threadLocalHashCode & (INITIAL_CAPACITY - 1);
+                    tab[i] = new Entry(myThreadLocal, value);
+                    map.put(t, tab);
+                }
             } finally {
                 w.unlock();
             }
         }
 
-        public void remove() {
+        public void remove(MyThreadLocal<?> myThreadLocal) {
             w.lock();
             try {
                 Thread t = Thread.currentThread();
-                //int i = t.getName().hashCode();
+                Entry[] tab = map.get(t);
+                int len = tab.length;
+                int i = myThreadLocal.threadLocalHashCode & (len - 1);
+                for (Entry e = tab[i]; e != null; e = tab[i = nextIndex(i, len)]) {
+                    if (e.get() == myThreadLocal) {
+                        e.clear();
+                    }
+                }
                 map.remove(t);
             } finally {
                 w.unlock();
+            }
+        }
+
+        private static int nextIndex(int i, int len) {
+            return ((i + 1 < len) ? i + 1 : 0);
+        }
+
+        private Entry[] resize(Entry[] table) {
+            int oldLen = table.length;
+            int newLen = oldLen * 2;
+            Entry[] newTab = new Entry[newLen];
+            for (Entry e : table) {
+                if (e != null) {
+                    MyThreadLocal<?> k = e.get();
+                    if (k == null) {
+                        e.value = null;
+                    } else {
+                        int h = k.threadLocalHashCode & (newLen - 1);
+                        while (newTab[h] != null)
+                            h = nextIndex(h, newLen);
+                        newTab[h] = e;
+                    }
+                }
+            }
+            return newTab;
+        }
+
+        static class Entry extends WeakReference<MyThreadLocal<?>> {
+            Object value;
+
+            Entry(MyThreadLocal<?> k, Object v) {
+                super(k);
+                value = v;
             }
         }
     }
--- src/TestMyThreadLocal.java
+++ src/TestMyThreadLocal.java
@@ -2,15 +2,30 @@
 public class TestMyThreadLocal {
 
     public static void main(String[] args) throws InterruptedException {
+
         MyThreadLocal<String> myThL = new MyThreadLocal<>();
+        MyThreadLocal<String> myThL1 = new MyThreadLocal<>();
+        MyThreadLocal<String> myThL2 = new MyThreadLocal<>();
         myThL.set(Thread.currentThread().getName());
         class MyRunnable implements Runnable {
             @Override
             public void run() {
                 myThL.set(Thread.currentThread().getName());
+                myThL2.set(Thread.currentThread().getName() + " myThL2");
+                myThL1.set(Thread.currentThread().getName() + " myThL1");
+
+                try {
+                    Thread.sleep(300);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName() + " myThL2 value: " + myThL2.get());
                 System.out.println(Thread.currentThread().getName() + " value: " + myThL.get());
+                System.out.println(Thread.currentThread().getName() + " myThL1 value: " + myThL1.get());
+
             }
         }
+
         /////////////////////////////////////////////////////////////////////////////////////////
         Thread t1 = new Thread(new MyRunnable());
         Thread t2 = new Thread(new MyRunnable());
@@ -38,6 +53,7 @@ public class TestMyThreadLocal {
         t7.join();
         t8.join();
         System.out.println(myThL.get());
+        myThL.remove();
         System.out.println(myThL.get());
     }
 }
